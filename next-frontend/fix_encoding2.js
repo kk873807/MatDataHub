29fded833c78a19f99a81962b103d14f8ac6eaa3
Kishan <kishan@matdatahub.com>
2026-09-15 12:09:56 +0530

    chore: massive cleanup of temporary script files, old backups, and empty folders; move docs to docs/

--- next-frontend/fix_encoding2.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/InteractiveFeatures.tsx', 'utf8');
-content = content.replace(/g\/cm./g, 'g/cm3');
-fs.writeFileSync('src/components/InteractiveFeatures.tsx', content);
--- next-frontend/fix_remove_dashboard_demo.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-// Remove PageDemo import and dashboard demo from dashboard page
-let dashboard = fs.readFileSync('src/app/dashboard/page.tsx', 'utf8');
-dashboard = dashboard.replace(/import { PageDemo } from "@\/components\/demos\/PageDemo";\n/, '');
-dashboard = dashboard.replace(/import { SimulatedDashboardDemo } from "@\/components\/demos\/SimulatedDashboardDemo";\n/, '');
-dashboard = dashboard.replace(/\{\/\* Interactive Demo \*\/\}\n\s*<PageDemo pageKey="dashboard" title="See how your Dashboard works">\n\s*<SimulatedDashboardDemo \/>\n\s*<\/PageDemo>\n\n\s*/g, '');
-fs.writeFileSync('src/app/dashboard/page.tsx', dashboard);
-console.log('Removed dashboard demo');
